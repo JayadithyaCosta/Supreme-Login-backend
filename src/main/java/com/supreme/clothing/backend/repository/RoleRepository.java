64ea4264587a1f3c52cff04ced1443d7eac5b698
Chathuranga Costa <it17088138@my.sliit.lk>
2021-03-08 13:11:39 +0530

    Initial push after backend created and completed. But Spring jpa wont create the user table automatically

---
+++ src/main/java/com/supreme/clothing/backend/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.supreme.clothing.backend.repository;
+
+import com.supreme.clothing.backend.models.ERole;
+import com.supreme.clothing.backend.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
